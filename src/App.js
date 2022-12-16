c4e39fd6444f77604c2e7938a10f1ff46866cc48
MohammadInamdar <inamdarmohammad4gmail.com>
2022-12-16 22:22:50 +0530

    create navbar and textform and set props for desinging purpose

--- src/App.js
+++ src/App.js
@@ -32,18 +32,18 @@ function App() {
                 </a>
               </li>
             </ul>
-            <form class="d-flex flex-end " role="search">
-              <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
           </div>
+          <form class="d-flex flex-end " role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
         </div>
       </nav>
     </>
