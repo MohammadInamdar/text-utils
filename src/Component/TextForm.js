45333d8a5a780e476a2bc0488a446c81786c8e2a
MohammadInamdar <inamdarmohammad4gmail.com>
2022-12-16 23:34:12 +0530

    convert text in uppercase using string method

--- src/Component/TextForm.js
+++ src/Component/TextForm.js
@@ -1,15 +1,34 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "../Component/All.css";
 const TextForm = (props) => {
+  const handleUpClick = () => {
+    // using toUpperCase string method chage text
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  //   Handle for event for use event.target.value
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  //   use for usestate for settext
+  const [text, setText] = useState("Enter Your Text here");
   return (
     <>
+      {/* props add into app.js  */}
       <h1>{props.heading}</h1>
-      <div class="mb-3">
+      <div className="mb-3">
         <textarea
-          class="form-control"
-          id="exampleFormControlTextarea1"
-          rows="3"
+          className="form-control"
+          id="myBox"
+          rows="8"
+          value={text}
+          onChange={handleOnChange}
         ></textarea>
+        <button className="btn btn-primary my-3 btn-6" onClick={handleUpClick}>
+          <span>UpperCase</span>
+        </button>
       </div>
     </>
   );
